0ebdc1ba5106009e80a0f9d98ec4d7acba992d0b
jeffersonfparil <jeffersonparil@gmail.com>
2025-03-18 14:53:45 +1100

    fixing doctest instantiation

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,7 @@ Pkg.add(url = "https://github.com/GenomicBreeding/GBCore.jl")
 Pkg.develop(url = "https://github.com/GenomicBreeding/GBPlotsInteractive.jl")
 Pkg.add("Documenter")
 using Documenter
-using GBCore
 using GBPlotsInteractive
-using StatsBase, MultivariateStats, Distributions, LinearAlgebra, DataFrames
-using UnicodePlots
-using GLMakie, ColorSchemes
 
 DocMeta.setdocmeta!(GBPlotsInteractive, :DocTestSetup, :(using GBPlotsInteractive); recursive = true)
 
